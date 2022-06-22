c750aa39e2f6f92284069fd032039866b1bac498
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-22 16:17:24 -0300

    concluido

--- Faturamento Diario.py
+++ Faturamento Diario.py
@@ -1,3 +1,4 @@
+#! Notas.......... Arquivo de faturamento disponibilizado é {dados.json}, para utiliza-lo deixe no mesmo diretorio do script.
 import json
 
 f = open('dados.json')
@@ -28,16 +29,27 @@ for item in listOfValues:
 
 maior = max(new_map)
 menor = min(new_map)
-print(("O menor valor de faturamento encontrado foi, R$:%f ") % (menor))
-print(("O maior valor de faturamento encontrado foi, R$:%f ") % (maior))
+print(("O menor valor de faturamento encontrado foi, \033[1;32mR$:%f\033[m") % (menor))
+print(("O maior valor de faturamento encontrado foi, \033[1;32mR$:%f\033[m") % (maior))
 
 # Descobrir media mensal
 soma_valores = sum(listOfValues)
 valor_dias = len(new_map)
 media = soma_valores / valor_dias
-print(("A média mensal calculada foi, R$:%f") % (media))
-# R$:20865.370167
+print(("A média mensal calculada foi, \033[1;32mR$:%f\033[m") % (media))
+
+print('\n')
 
 #Descobrir os dias em que o valor de faturamento diário foi superior à media mensal
-if media < listOfValues:
-    print(listOfValues)
+media = int(media)
+valor_fatu =[]
+
+for i in new_map:
+    if i > media:
+        valor_fatu.append(i)
+    else:
+        pass
+
+num_dias = len(valor_fatu)
+print(("Total de dias em que o Faturamento foi superior à media mensal, foram de: \033[1;32m%d dias.\033[m")%(num_dias))
+
