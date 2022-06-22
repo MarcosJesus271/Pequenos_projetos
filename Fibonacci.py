5d7907e643c569337285d211f872b6b1c1a22854
Marcos Jesus <39844149+MarcosJesus271@users.noreply.github.com>
2022-06-22 12:46:02 -0300

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+fibo = [1,1]
+i = 0
+verf = int(input("Digite o número que deseja pesquisar: "))
+num = int(input("Digite em quantos termos da sequencia devo pesquisar: "))
+
+while num > len(fibo):
+	fibo.append(fibo[i] + fibo[i+1])
+	i+=1
+
+lista = fibo
+print("\n"+lista+"\n")
+
+try:
+    if lista.index(verf):
+        print('Encontrado! O número %d pertence a sequencia Fibonacci'%(verf))
+except:
+    print("Não encontrado! O número %d não pertence a sequencia Fibonacci ou altere a quantidade de termos para uma nova busca" % (verf))
